e2b84ea059eb3883a5b48192879bc249c753e7d2
QMWebDesigns <qmwebdesigns24@gmail.com>
2025-10-02 09:46:12 +0000

    AdminAnalytics

---
+++ src/components/Admin/AdminAnalytics.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import AdminAnalytics from './AdminAnalytics';
+
+const AdminDashboard: React.FC = () => {
+  return <AdminAnalytics />;
+};
+
+export default AdminDashboard;
